6e9ba25c064bce624275953ab9b75f32298ac8d1
Karim Kabirov <karim3487@gmail.com>
2025-05-19 18:16:50 +0600

    Fix error with creation progress

--- apps/api/v1/progress/views.py
+++ apps/api/v1/progress/views.py
@@ -2,9 +2,10 @@ from drf_spectacular.utils import OpenApiParameter, extend_schema
 from rest_framework import generics, status
 from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from apps.api.v1.progress import serializers
-from apps.api.v1.progress.serializers import ProgressSerializer
+from apps.common.pagination import CustomPagination
 from apps.plans.models import Progress
 from apps.profiles.models import ClientProfile
 
@@ -23,43 +24,43 @@ class ProgressCreateView(generics.CreateAPIView):
     serializer_class = serializers.ProgressSerializer
 
 
-@extend_schema(
-    request=serializers.ProgressCreateByTelegramSerializer,
-    responses=serializers.ProgressSerializer,
-    description="Create a progress record using telegram_id instead of profile_id.",
-)
-class ProgressCreateByTelegramView(generics.CreateAPIView):
-    queryset = Progress.objects.all()
-    serializer_class = serializers.ProgressCreateByTelegramSerializer
+class ProgressByTelegramView(APIView):
+    pagination_class = CustomPagination
 
-    def create(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+    @extend_schema(
+        summary="Get progress by user Telegram ID",
+        parameters=[
+            OpenApiParameter(
+                name="telegram_id",
+                description="User Telegram ID",
+                required=True,
+                type=int,
+                location=OpenApiParameter.QUERY,
+            )
+        ],
+        responses=serializers.ProgressSerializer(many=True),
+    )
+    def get(self, request, *args, **kwargs):
+        telegram_id = request.query_params.get("telegram_id")
+        profile = get_object_or_404(
+            ClientProfile, telegram_ids__telegram_id=telegram_id
+        )
+        queryset = Progress.objects.filter(client_profile=profile).order_by("-date")
+        paginator = self.pagination_class()
+        page = paginator.paginate_queryset(queryset, request, view=self)
+
+        serializer = serializers.ProgressSerializer(page, many=True)
+        return paginator.get_paginated_response(serializer.data)
+
+    @extend_schema(
+        request=serializers.ProgressCreateByTelegramSerializer,
+        responses=serializers.ProgressSerializer,
+        description="Create a progress record using telegram_id instead of profile_id.",
+    )
+    def post(self, request, *args, **kwargs):
+        serializer = serializers.ProgressCreateByTelegramSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         progress_instance = serializer.save()
 
         output_serializer = serializers.ProgressSerializer(progress_instance)
         return Response(output_serializer.data, status=status.HTTP_201_CREATED)
-
-
-@extend_schema(
-    summary="Get progress by user Telegram ID",
-    parameters=[
-        OpenApiParameter(
-            name="telegram_id",
-            description="User Telegram ID",
-            required=True,
-            type=int,
-            location=OpenApiParameter.QUERY,
-        )
-    ],
-    responses=ProgressSerializer(many=True),
-)
-class ProgressListByTelegramView(generics.ListAPIView):
-    serializer_class = serializers.ProgressSerializer
-
-    def get_queryset(self):
-        telegram_id = self.request.query_params.get("telegram_id")
-        profile = get_object_or_404(
-            ClientProfile, telegram_ids__telegram_id=telegram_id
-        )
-        return Progress.objects.filter(client_profile=profile).order_by("-date")
